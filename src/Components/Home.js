0fa3927c35660a47824383f08f434184a12fc752
Safdar Hussain <safdarhussainmangnejo@gmail.com>
2023-05-02 17:55:26 +0500

    solve error

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,8 @@
-import Landing from './Landing';
-import Navbar from './Navbar';
-import DisplayImages from './DisplayImages';
 import CreditImage from './CreditsImage'
 import Footer from './Footer';
 import ResponsiveNavbar from './ResponsiveNavbar';
 import ShowImages from './ShowImages';
-import Cards from './Cards';
-import ResponsiveCard from './ResponsiveCard';
-import CardsRow from './CardsRow';
-import ShowCards from './ShowCards'
+
 
 const Home = () => {
     
